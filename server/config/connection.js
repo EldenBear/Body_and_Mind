9efcfcfc939e991c9f1cca6e75e4c3fd44a85634
Tony <tonyguarino1489@gmail.com>
2023-07-28 00:00:08 -0400

    Configured to use environment files and deleted db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,15 +1,7 @@
-// config/connection.js
-
 const mongoose = require('mongoose');
 
-// Replace 'your_database_uri' with your actual MongoDB connection URI
-const databaseURI = 'mongodb://localhost:27017/your_database_name_tbd';
-
-// Connect to the MongoDB database
-mongoose.connect(databaseURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('Connected to MongoDB database!');
   })
@@ -17,8 +9,4 @@ mongoose.connect(databaseURI, {
     console.error('Error connecting to MongoDB:', error);
   });
 
-  module.exports = mongoose.connection;
-
-
-
-
+module.exports = mongoose.connection;

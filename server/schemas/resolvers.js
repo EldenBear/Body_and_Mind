8b7b335fd3e8534ac72345661b81bcf9017c1807
Tony <tonyguarino1489@gmail.com>
2023-07-30 14:21:17 -0400

    Started the query resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,19 @@
-// Need models to be merged
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+// Need the sign token utility when finsihed
+
+const resolvers = {
+  Query: {
+    me: async (_, __, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('comments');
+      } else {
+        throw new AuthenticationError();
+      }
+    },
+  },
+
+  Mutation: {},
+};
+
+module.exports = resolvers;

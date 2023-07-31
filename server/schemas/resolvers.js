241b9b5f045dd3d2d04675e6652f6bb41925087d
Tony <tonyguarino1489@gmail.com>
2023-07-31 19:58:26 -0400

    updated

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,29 @@
+const { User } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
-const { User, commentSchema } = require('../models');
-const { signToken } = require('../utils/auth');
+// Need the sign token utility when finsihed
 
 const resolvers = {
+  Query: {
+    me: async (_, __, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('comments');
+      } else {
+        throw new AuthenticationError();
+      }
+    },
+  },
+  register: async (_, { name, email, password }) => {
+    try {
+      const newUser = User.create({ name, email, password });
+      //   const token = have to wait for token
+      //   return { newUser, token };
+    } catch (err) {
+      console.error(err);
+      throw new AuthenticationError();
+    }
+  },
 
-  };
-  
-  module.exports = resolvers;
+  Mutation: {},
+};
+
+module.exports = resolvers;

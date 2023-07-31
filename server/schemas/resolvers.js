a180b5ce11ea4ef23293c07e4e422aa59e7034a2
Xandi <santosal2018@gmail.com>
2023-07-31 18:56:10 -0400

    add login resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -18,7 +18,32 @@ const resolvers = {
             // Return an `Auth` object that consists of the signed token and user's information
             return { token, user };
           },
+
+          login: async (parent, { email, password }) => {
+            // Look up the user by the provided email address. Since the `email` field is unique, we know that only one person will exist with that email
+            const user = await User.findOne({ email });
+      
+            // If there is no user with that email address, return an Authentication error stating so
+            if (!user) {
+              throw new AuthenticationError('No user found with this email address');
+            }
+      
+            // If there is a user found, execute the `isCorrectPassword` instance method and check if the correct password was provided
+            const correctPw = await user.isCorrectPassword(password);
+      
+            // If the password is incorrect, return an Authentication error stating so
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            // If email and password are correct, sign user into the application with a JWT
+            const token = signToken(user);
+      
+            // Return an `Auth` object that consists of the signed token and user's information
+            return { token, user };
+          },
     },
+    
   };
   
   module.exports = resolvers;

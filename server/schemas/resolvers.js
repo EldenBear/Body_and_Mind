fe5498b4c464a77d7f10a7ea987a78579e16e6e6
Xandi <santosal2018@gmail.com>
2023-07-31 18:53:32 -0400

    add 'me' query and 'addUser' mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,24 @@
-// Need models to be merged
+const { AuthenticationError } = require('apollo-server-express');
+const { User, commentSchema } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, { username }) => {
+            return User.findOne({ username }).populate()
+        }
+    },
+  
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            // First we create the user
+            const user = await User.create({ username, email, password });
+            // To reduce friction for the user, we immediately sign a JSON Web Token and log the user in after they are created
+            const token = signToken(user);
+            // Return an `Auth` object that consists of the signed token and user's information
+            return { token, user };
+          },
+    },
+  };
+  
+  module.exports = resolvers;

8378bb85bc698a9bb00f3915ead841e7b6a6e044
Austin1094 <Austin.Adams1094@gmail.com>
2023-07-31 21:34:18 -0400

    Styled both login and signup

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -35,27 +35,32 @@ const Signup = () => {
     };
 
     return (
-        <Box p={4} maxWidth="400px" margin="0 auto">
-            <form onSubmit={handleSubmit}>
-                <FormControl id="email" mb={4}>
-                    <FormLabel>Username</FormLabel>
-                    <Input type="username" value={username} onChange={(e) => setUsername(e.target.value)} />
-                </FormControl>
-                <FormControl id="password" mb={4}>
-                    <FormLabel>Password</FormLabel>
-                    <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                </FormControl>
-                <FormControl id="confirmPassword" mb={4}>
-                    <FormLabel>Confirm Password</FormLabel>
-                    <Input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-                </FormControl>
-                <Button type="submit" colorScheme="green">Confirm Signup</Button>
+        <div className="signup-box" style={{ backgroundColor: '#d9f3f6' }}>
+            <div className="centered-box">
+                <h1 style={{ fontSize: '2rem', fontWeight: 'bold', textAlign: 'center', marginBottom: '20px' }}>Body And Mind</h1>
+                <Box p={4} maxWidth="400px" margin="0 auto" className='signup-container'>
+                    <form onSubmit={handleSubmit}>
+                        <FormControl id="email" mb={4}>
+                            <FormLabel>Username</FormLabel>
+                            <Input type="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                        </FormControl>
+                        <FormControl id="password" mb={4}>
+                            <FormLabel>Password</FormLabel>
+                            <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        </FormControl>
+                        <FormControl id="confirmPassword" mb={4}>
+                            <FormLabel>Confirm Password</FormLabel>
+                            <Input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                        </FormControl>
+                        <Button type="submit" colorScheme="green">Confirm Signup</Button>
 
-                <Link to="/">
-                    <Button colorScheme="blue" mt={4}>Back to Login</Button>
-                </Link>
-            </form>
-        </Box>
+                        <Link to="/">
+                            <Button colorScheme="blue" mt={4}>Back to Login</Button>
+                        </Link>
+                    </form>
+                </Box>
+            </div>
+        </div>
     );
 };
 

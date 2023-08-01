3560abcc6d955bbf6ce3a8f7b4ffa830c0c2cfd8
jb1756 <juliusbags04@yahoo.com>
2023-07-31 20:54:38 -0400

    added REGISTER_USER, LOGIN_USER, ADD_BIO, GET_EXERCISES without logic

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,23 +1,56 @@
 import { gql } from '@apollo/client';
 
-export const CREATE_MATCHUP = gql`
-  mutation createMatchup($tech1: String!, $tech2: String!) {
-    createMatchup(tech1: $tech1, tech2: $tech2) {
+export const REGISTER_USER = gql`
+mutation register($name: String!, $email: String!, $password: String!) {
+  register(name: $name, email: $email, password: $password) {
+    token
+    user {
       _id
-      tech1
-      tech2
+      name
+      email
+      age
+      aboutme
+      hobbies
+      gender
+      profilePicture
+      activityLevel
+      comments {
+        content
+        createdAt
+      }
     }
   }
+}
 `;
 
-export const CREATE_VOTE = gql`
-  mutation createVote($_id: String!, $techNum: Int!) {
-    createVote(_id: $_id, techNum: $techNum) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
-    }
-  }
+export const LOGIN_USER = gql`
+
 `;
+
+export const ADD_BIO = gql`
+
+`; 
+
+export const 
+
+// export const CREATE_MATCHUP = gql`
+//   mutation createMatchup($tech1: String!, $tech2: String!) {
+//     createMatchup(tech1: $tech1, tech2: $tech2) {
+//       _id
+//       tech1
+//       tech2
+//     }
+//   }
+// `;
+
+// export const CREATE_VOTE = gql`
+//   mutation createVote($_id: String!, $techNum: Int!) {
+//     createVote(_id: $_id, techNum: $techNum) {
+//       _id
+//       tech1
+//       tech2
+//       tech1_votes
+//       tech2_votes
+//     }
+//   }
+// `;

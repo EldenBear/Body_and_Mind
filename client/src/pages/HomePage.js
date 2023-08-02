58e1735ef4be042bba3bd86ef3f7564df6579c0b
Donte Taylor <dontet1995@gmail.com>
2023-08-02 17:07:54 -0400

    Added add post feature

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -24,6 +24,17 @@ const HomePage = () => {
   const [isMobile, setIsMobile] = React.useState(window.innerWidth < 900);
   const [avatarURL, setAvatarURL] = React.useState('https://bit.ly/broken-link');
   const [currentDrawer, setCurrentDrawer] = React.useState(null);
+  const [posts, setPosts] = React.useState([
+    {
+      id: 1,
+      name: "John Smith",
+      userTitle: "Developer",
+      postText: "Sample text",
+      imageURL: "https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    }
+  ]);
+  const [addPostDesc, setAddPostDesc] = React.useState("");
+  const [addPostImage, setAddPostImage] = React.useState("");
 
   const { isOpen, onOpen, onClose, firstField, } = useDisclosure();
   const commentDrawer = "comment";
@@ -40,6 +51,42 @@ const HomePage = () => {
   function openDrawer(drawer) {
     setCurrentDrawer(drawer);
     onOpen();
+  };
+
+  function onChangePostDesc(e) {
+    setAddPostDesc(e.target.value)
+  };
+
+  function onChangePostImage(e) {
+    setAddPostImage(e.target.value)
+  };
+
+  function onSubmit() {
+    const newPost = {
+      id: 1,
+      name: "John Smith",
+      userTitle: "Developer",
+      postText: addPostDesc,
+      imageURL: addPostImage,
+    };
+    setPosts([...posts, newPost]);
+    onClose()
+  };
+
+  const renderPosts = () => {
+    return (
+      posts.map(post => {
+        return (
+          <Post
+          name={post.name}
+          userTitle={post.userTitle}
+          imageURL={post.imageURL}
+          postText={post.postText}
+          onClickComment={() => openDrawer(commentDrawer)}
+        ></Post>
+        )
+      })
+    )
   }
 
   return (
@@ -47,12 +94,7 @@ const HomePage = () => {
     <Button className="addPostButton" position={"fixed"} colorScheme="primary" onClick={() => openDrawer(postDrawer)}>+ Add Post</Button>
      <Navigation isMobile={isMobile} avatarURL={avatarURL}></Navigation>
       <div className="main">
-        <Post
-          name="John Smith"
-          userTitle="Web Developer"
-          postText="Sample post text goes here"
-          onClickComment={() => openDrawer(commentDrawer)}
-        ></Post>
+       {renderPosts()}
       </div>
       <Drawer onClose={onClose} isOpen={isOpen && currentDrawer === commentDrawer} size={"lg"}>
         <DrawerOverlay />
@@ -118,25 +160,26 @@ const HomePage = () => {
           </DrawerHeader>
           <DrawerBody>
               <Box>
+                <FormLabel htmlFor='desc'>Description</FormLabel>
+                <Textarea id='desc' onChange={onChangePostDesc} />
+              </Box>
+              <Box>
                 <FormLabel htmlFor='url'>Image URL</FormLabel>
                 <InputGroup>
                   <Input
+                    onChange={onChangePostImage}
                     type='url'
                     id='url'
                     placeholder='Please enter image url for post'
                   />
                 </InputGroup>
               </Box>
-              <Box>
-                <FormLabel htmlFor='desc'>Description</FormLabel>
-                <Textarea id='desc' />
-              </Box>
           </DrawerBody>
           <DrawerFooter borderTopWidth='1px'>
             <Button variant='outline' mr={3} onClick={onClose}>
               Cancel
             </Button>
-            <Button colorScheme='blue'>Submit</Button>
+            <Button colorScheme='blue' onClick={onSubmit}>Submit</Button>
           </DrawerFooter>
         </DrawerContent>
       </Drawer>

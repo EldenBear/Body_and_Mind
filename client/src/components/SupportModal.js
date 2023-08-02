7bd45fe361ce4d2ddb2c03df744468f8732ad569
Austin1094 <Austin.Adams1094@gmail.com>
2023-08-02 19:15:55 -0400

    Added blur feature to Modal

--- client/src/components/SupportModal.js
+++ client/src/components/SupportModal.js
@@ -18,26 +18,33 @@ const SupportModal = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <>
-      <Button onClick={onOpen} colorScheme="secondary" color="black" className="supportButton">You're Not Alone</Button>
+      <Button onClick={onOpen} colorScheme="secondary" color="black" className="supportButton">
+        You're Not Alone
+      </Button>
 
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Support Hotlines</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <p>
-              <b>Suicide and Crisis Lifeline:</b> 988 <br />
-              <b>Anorexia Nervosa & Associated Disorders (ANAD) Helpline:</b> 1-888-375-7767 <br />
-              <b>Substance Abuse and Mental Health Services Administration Helpline:</b> 1-800-662-4357 <br />
-              <b>The Trevor Project LGBTQ+:</b> 1-866-488-7386 <br />
-              <b>National Domestic Violence Hotline:</b> 1-800-799-7233
-            </p>
-          </ModalBody>
-          <ModalFooter>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+      {isOpen && (
+        <>
+          <div className="blur-overlay" onClick={onClose} />
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Support Hotlines</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <p>
+                  <b>Suicide and Crisis Lifeline:</b> 988 <br />
+                  <b>Anorexia Nervosa & Associated Disorders (ANAD) Helpline:</b> 1-888-375-7767 <br />
+                  <b>Substance Abuse and Mental Health Services Administration Helpline:</b> 1-800-662-4357 <br />
+                  <b>The Trevor Project LGBTQ+:</b> 1-866-488-7386 <br />
+                  <b>National Domestic Violence Hotline:</b> 1-800-799-7233
+                </p>
+              </ModalBody>
+              <ModalFooter>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </>
+      )}
     </>
   )
 };

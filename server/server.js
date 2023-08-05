c5b87dc60cb3c957a8c520e3e931461888b221d0
Donte Taylor <dontet1995@gmail.com>
2023-08-05 11:10:10 -0400

    test

--- server/server.js
+++ server/server.js
@@ -26,6 +26,9 @@ if (process.env.NODE_ENV === 'production') {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
